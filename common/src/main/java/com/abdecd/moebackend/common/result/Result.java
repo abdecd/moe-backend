9eaeba43b05f86ac5b4db5a6c818e77e62303b8d
abdecd <abdecd@users.noreply.github.com>
2024-05-09 17:34:16 +0800

    refactor: 调整 Result 类

--- common/src/main/java/com/abdecd/moebackend/common/result/Result.java
+++ common/src/main/java/com/abdecd/moebackend/common/result/Result.java
@@ -1,38 +1,66 @@
 package com.abdecd.moebackend.common.result;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 
-@Data
-public class Result<T> {
-    private int code; //编码：200成功，0和其它数字为失败
-    private String msg = ""; //错误信息
-    private T data; //数据
+public class Result<T> extends ResponseEntity<Result.ResultBody<T>> {
+
+    /**
+     * @code 编码：200成功, 其它数字为失败
+     * @msg 错误信息
+     * @data 数据
+     */
+    @SuppressWarnings("all")
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public static class ResultBody<T> {
+        private int code = 200;
+        private String msg = "";
+        private T data;
+    }
+
+    /**
+     * http状态码与响应体相同
+     */
+    public Result(ResultBody<T> body) {
+        super(body, HttpStatusCode.valueOf(body.getCode()));
+    }
+
+    public Result(ResultBody<T> body, int code) {
+        super(body, HttpStatusCode.valueOf(code));
+    }
+
+//    public ResponseEntity<Result<T>> getResponseEntity() {
+//        return ResponseEntity.status(this.code).body(this);
+//    }
 
     public static Result<String> success() {
-        Result<String> result = new Result<>();
-        result.code = 200;
-        result.data = "ok";
-        return result;
+        var result = new ResultBody<String>();
+        result.setData("ok");
+        return new Result<>(result);
     }
 
     public static <T> Result<T> success(T object) {
-        Result<T> result = new Result<>();
-        result.data = object;
-        result.code = 200;
-        return result;
+        var result = new ResultBody<T>();
+        result.setData(object);
+        return new Result<>(result);
     }
 
     public static Result<String> error(String msg) {
-        Result<String> result = new Result<>();
-        result.msg = msg;
-        result.code = 400;
-        return result;
+        var result = new ResultBody<String>();
+        result.setMsg(msg);
+        result.setCode(400);
+        return new Result<>(result);
     }
 
     public static Result<String> error(int code, String msg) {
-        Result<String> result = new Result<>();
-        result.msg = msg;
-        result.code = code;
-        return result;
+        var result = new ResultBody<String>();
+        result.setMsg(msg);
+        result.setCode(code);
+        return new Result<>(result);
     }
 }

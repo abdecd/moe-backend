a758ad94de67d487c988a3131f1cc18500668bca
abdecd <abdecd@users.noreply.github.com>
2024-05-12 17:06:31 +0800

    feat: 用户历史记录

--- business/src/main/java/com/abdecd/moebackend/business/service/PlainUserHistoryService.java
+++ business/src/main/java/com/abdecd/moebackend/business/service/PlainUserHistoryService.java
@@ -3,6 +3,8 @@ package com.abdecd.moebackend.business.service;
 import com.abdecd.moebackend.business.dao.entity.PlainUserHistory;
 import com.abdecd.moebackend.business.pojo.dto.plainuser.AddHistoryDTO;
 import com.abdecd.moebackend.business.pojo.vo.plainuser.HistoryVO;
+import com.abdecd.moebackend.business.pojo.vo.plainuser.UploaderVO;
+import com.abdecd.moebackend.business.service.videogroup.VideoGroupServiceBase;
 import com.abdecd.moebackend.common.constant.RedisConstant;
 import com.abdecd.moebackend.common.result.PageVO;
 import com.abdecd.tokenlogin.common.context.UserContext;
@@ -11,6 +13,8 @@ import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class PlainUserHistoryService {
@@ -24,6 +28,8 @@ public class PlainUserHistoryService {
     private RedissonClient redissonClient;
     @Autowired
     private PlainUserHistoryBatchSaver plainUserHistoryBatchSaver;
+    @Autowired
+    private VideoGroupServiceBase videoGroupServiceBase;
 
     public PageVO<HistoryVO> getHistory(Integer page, Integer pageSize) {
         var list = redisTemplate.opsForList().range(
@@ -38,22 +44,21 @@ public class PlainUserHistoryService {
     }
 
     public HistoryVO formHistoryVO(PlainUserHistory plainUserHistory) {
-        return null;
-//        var uploader = plainUserService.getPlainUserDetail(plainUserHistory.getUserId());
-//        var video = videoService.getVideo(plainUserHistory.getVideoId());
-//        var videoGroup = videoGroupService.getVideoGroup(plainUserHistory.getVideoGroupId());
-//
-//        var uploaderVO = new UploaderVO()
-//                .setId(uploader.getUserId())
-//                .setAvatar(uploader.getAvatar())
-//                .setNickname(uploader.getNickname());
-//        return new HistoryVO()
-//                .setUploader(uploaderVO)
-//                .setVideoGroupId(videoGroup.getVideoGroupId())
-//                .setVideoGroupTitle(videoGroup.getVideoGroupTitle())
-//                .setVideoGroupCover(videoGroup.getVideoGroupCover())
-//                .setVideoId(video.getId())
-//                .setVideoTitle(video.getTitle());
+        var uploader = plainUserService.getPlainUserDetail(plainUserHistory.getUserId());
+        var video = videoService.getVideo(plainUserHistory.getVideoId());
+        var videoGroup = videoGroupServiceBase.getVideoGroupInfo(plainUserHistory.getVideoGroupId());
+
+        var uploaderVO = new UploaderVO()
+                .setId(uploader.getUserId())
+                .setAvatar(uploader.getAvatar())
+                .setNickname(uploader.getNickname());
+        return new HistoryVO()
+                .setUploader(uploaderVO)
+                .setVideoGroupId(videoGroup.getVideoGroupId())
+                .setVideoGroupTitle(videoGroup.getTitle())
+                .setVideoGroupCover(videoGroup.getCover())
+                .setVideoId(video.getId())
+                .setVideoTitle(video.getTitle());
     }
 
     @Nullable
@@ -69,54 +74,54 @@ public class PlainUserHistoryService {
     }
 
     public void addHistory(AddHistoryDTO addHistoryDTO) {
-//        var video = videoService.getVideo(addHistoryDTO.getVideoId());
-//        var videoGroup = videoGroupService.getVideoGroup(video.getVideoGroupId());// todo 返回多态基类 基类不缓存 对应的子类VO缓存
-//        var entity = addHistoryDTO.toEntity(videoGroup.getUserId(), videoGroup.getId());
-//
-//        // add to redis
-//        var lock = redissonClient.getLock(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId());
-//        lock.lock();
-//        try {
-//            // delete old record
-//            var list = redisTemplate.opsForList().range(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), 0, -1);
-//            if (list == null) list = new ArrayList<>();
-//            for (var item : list) {
-//                if (item.getVideoId().equals(addHistoryDTO.getVideoId())) {
-//                    redisTemplate.opsForList().remove(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), 0, item);
-//                    break;
-//                }
-//            }
-//            // add new record
-//            redisTemplate.opsForList().leftPush(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), entity);
-//            redisTemplate.opsForList().trim(
-//                    RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(),
-//                    0,
-//                    RedisConstant.PLAIN_USER_HISTORY_SIZE
-//            );
-//        } finally {
-//            lock.unlock();
-//        }
-//        // add to db
-//        plainUserHistoryBatchSaver.add(entity);
+        var video = videoService.getVideo(addHistoryDTO.getVideoId());
+        var videoGroup = videoGroupServiceBase.getVideoGroupInfo(video.getVideoGroupId());
+        var entity = addHistoryDTO.toEntity(UserContext.getUserId(), videoGroup.getVideoGroupId());
+
+        // add to redis
+        var lock = redissonClient.getLock(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId());
+        lock.lock();
+        try {
+            // delete old record
+            var list = redisTemplate.opsForList().range(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), 0, -1);
+            if (list == null) list = new ArrayList<>();
+            for (var item : list) {
+                if (item.getVideoId().equals(addHistoryDTO.getVideoId())) {
+                    redisTemplate.opsForList().remove(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), 0, item);
+                    break;
+                }
+            }
+            // add new record
+            redisTemplate.opsForList().leftPush(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), entity);
+            redisTemplate.opsForList().trim(
+                    RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(),
+                    0,
+                    RedisConstant.PLAIN_USER_HISTORY_SIZE
+            );
+        } finally {
+            lock.unlock();
+        }
+        // add to db
+        plainUserHistoryBatchSaver.add(entity);
     }
 
     public void deleteHistory(long[] videoGroupIds) {
-//        // delete from redis
-//        var lock = redissonClient.getLock(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId());
-//        lock.lock();
-//        try {
-//            // delete record
-//            var list = redisTemplate.opsForList().range(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), 0, -1);
-//            if (list == null) list = new ArrayList<>();
-//            for (var item : list) {
-//                for (var id : videoGroupIds) {
-//                    if (item.getVideoGroupId().equals(id)) {
-//                        redisTemplate.opsForList().remove(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), 0, item);
-//                    }
-//                }
-//            }
-//        } finally {
-//            lock.unlock();
-//        }
+        // delete from redis
+        var lock = redissonClient.getLock(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId());
+        lock.lock();
+        try {
+            // delete record
+            var list = redisTemplate.opsForList().range(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), 0, -1);
+            if (list == null) list = new ArrayList<>();
+            for (var item : list) {
+                for (var id : videoGroupIds) {
+                    if (item.getVideoGroupId().equals(id)) {
+                        redisTemplate.opsForList().remove(RedisConstant.PLAIN_USER_HISTORY + UserContext.getUserId(), 0, item);
+                    }
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
     }
 }

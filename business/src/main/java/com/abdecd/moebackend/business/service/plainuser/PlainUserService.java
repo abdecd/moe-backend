262682612cdc14a652993875cf21d6b56fbc1012
clt123haha <121712843+clt123haha@users.noreply.github.com>
2024-05-22 11:49:49 +0800

    Merge branch 'dev' into dev-clt

--- business/src/main/java/com/abdecd/moebackend/business/service/plainuser/PlainUserService.java
+++ business/src/main/java/com/abdecd/moebackend/business/service/plainuser/PlainUserService.java
@@ -1,8 +1,11 @@
 package com.abdecd.moebackend.business.service.plainuser;
 
 import com.abdecd.moebackend.business.common.exception.BaseException;
+import com.abdecd.moebackend.business.controller.base.CommonController;
 import com.abdecd.moebackend.business.dao.entity.PlainUserDetail;
+import com.abdecd.moebackend.business.dao.entity.VideoGroup;
 import com.abdecd.moebackend.business.dao.mapper.PlainUserDetailMapper;
+import com.abdecd.moebackend.business.dao.mapper.VideoGroupMapper;
 import com.abdecd.moebackend.business.pojo.dto.plainuser.UpdatePlainUserDTO;
 import com.abdecd.moebackend.business.service.fileservice.FileService;
 import com.abdecd.moebackend.common.constant.MessageConstant;
@@ -10,14 +13,16 @@ import com.abdecd.moebackend.common.constant.RedisConstant;
 import com.abdecd.tokenlogin.common.context.UserContext;
 import com.abdecd.tokenlogin.mapper.UserMapper;
 import com.abdecd.tokenlogin.pojo.entity.User;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -28,6 +33,12 @@ public class PlainUserService {
     private UserMapper userMapper;
     @Autowired
     private FileService fileService;
+    @Autowired
+    private CommonController commonController;
+    @Autowired
+    private VideoGroupMapper videoGroupMapper;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Cacheable(value = RedisConstant.PLAIN_USER_DETAIL, key = "#uid", unless = "#result == null")
     public PlainUserDetail getPlainUserDetail(Long uid) {
@@ -41,9 +52,14 @@ public class PlainUserService {
         String oldFileUrl = null;
         if (updatePlainUserDTO.getAvatar() != null) {
             try {
-                newFileUrl = fileService.uploadFile(updatePlainUserDTO.getAvatar(), "/user", "avatar" + UUID.randomUUID());
+                var result = commonController.uploadImg(updatePlainUserDTO.getAvatar());
+                if (Objects.requireNonNull(result.join().getBody()).getCode() != 200) throw new Exception();
+                var avatarBody = result.join().getBody();
+                if (avatarBody == null) throw new Exception();
+                var avatar = avatarBody.getData();
+                newFileUrl = fileService.changeTmpFileToStatic(avatar, "/user", "avatar-" + UUID.randomUUID() + avatar.substring(avatar.lastIndexOf('.')));
                 oldFileUrl = plainUserDetailMapper.selectById(UserContext.getUserId()).getAvatar();
-            } catch (IOException e) {
+            } catch (Exception e) {
                 throw new BaseException(MessageConstant.IMG_FILE_UPLOAD_FAIL);
             }
         }
@@ -53,10 +69,23 @@ public class PlainUserService {
                     .set(User::getNickname, updatePlainUserDTO.getNickname())
             );
         }
-        var entity = updatePlainUserDTO.toEntity();
+        var entity = updatePlainUserDTO.toEntity(UserContext.getUserId());
         if (newFileUrl != null) entity.setAvatar(newFileUrl);
         plainUserDetailMapper.updateById(entity);
         if (oldFileUrl != null) fileService.deleteFile(oldFileUrl);
+        // 清理缓存
+        clearVideoGroupCache(UserContext.getUserId());
+    }
+
+    private void clearVideoGroupCache(Long userId) {
+        var list = videoGroupMapper.selectList(new LambdaQueryWrapper<VideoGroup>()
+                .eq(VideoGroup::getUserId, userId)
+        );
+        if (list.isEmpty()) return;
+        for (var videoGroup : list) {
+            stringRedisTemplate.delete(RedisConstant.VIDEO_GROUP_CACHE.substring(0, RedisConstant.VIDEO_GROUP_CACHE.lastIndexOf("#")) + "::" + videoGroup.getId());
+            stringRedisTemplate.delete(RedisConstant.BANGUMI_VIDEO_GROUP_CACHE.substring(0, RedisConstant.BANGUMI_VIDEO_GROUP_CACHE.lastIndexOf("#")) + "::" + videoGroup.getId());
+        }
     }
 
     @SuppressWarnings("unused")

d94effda013207fc44285c2c940f2676883120ef
abdecd <abdecd@users.noreply.github.com>
2024-08-23 09:36:19 +0800

    chore: change constant to interface

--- common/src/main/java/com/abdecd/moebackend/common/constant/DTOConstant.java
+++ common/src/main/java/com/abdecd/moebackend/common/constant/DTOConstant.java
@@ -1,14 +1,14 @@
 package com.abdecd.moebackend.common.constant;
 
-public class DTOConstant {
-    public static final int PERSON_NAME_LENGTH_MAX = 40;
-    public static final int PERSON_NAME_LENGTH_MIN = 1;
-    public static final int PASSWORD_LENGTH_MAX = 344;
-    public static final int STRING_LENGTH_MAX = 200;
-    public static final int COMMENT_LENGTH_MAX = 4000;
-    public static final int FEEDBACK_LENGTH_MAX = 10000;
-    public static final int CAPTCHA_LENGTH = 4;
-    public static final int EMAIL_VERIFY_CODE_LENGTH = 6;
-    public static final String TAGS_REGEXP = "^[^;]+(?:;[^;]+)*$";
-    public static final String PERSON_NAME_REGEX = "^[^@ ]+$";
+public interface DTOConstant {
+    int PERSON_NAME_LENGTH_MAX = 40;
+    int PERSON_NAME_LENGTH_MIN = 1;
+    int PASSWORD_LENGTH_MAX = 344;
+    int STRING_LENGTH_MAX = 200;
+    int COMMENT_LENGTH_MAX = 4000;
+    int FEEDBACK_LENGTH_MAX = 10000;
+    int CAPTCHA_LENGTH = 4;
+    int EMAIL_VERIFY_CODE_LENGTH = 6;
+    String TAGS_REGEXP = "^[^;]+(?:;[^;]+)*$";
+    String PERSON_NAME_REGEX = "^[^@ ]+$";
 }
--- common/src/main/java/com/abdecd/moebackend/common/constant/VideoGroupConstant.java
+++ common/src/main/java/com/abdecd/moebackend/common/constant/VideoGroupConstant.java
@@ -1,7 +1,7 @@
 package com.abdecd.moebackend.common.constant;
 
-public class VideoGroupConstant {
-    public static final Byte COMMON_VIDEO_GROUP = 0;
-    public static final Byte ANIME_VIDEO_GROUP = 1;
-    public static final Double DEFAULT_WEIGHT = 1.0;
+public interface VideoGroupConstant {
+    Byte COMMON_VIDEO_GROUP = 0;
+    Byte ANIME_VIDEO_GROUP = 1;
+    Double DEFAULT_WEIGHT = 1.0;
 }
